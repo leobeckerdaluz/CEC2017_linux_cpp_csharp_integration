9fa0367bc09994b56d6880ec3c105243d1e5bf81
leobeckerdaluz <leobeckerdaluz@gmail.com>
2022-02-02 15:15:42 -0300

    cpp code compiled + generate .so + C# function call

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-Linux_C-dotnet_IntegrateCCode
+## Tutorial
+
+### Baseado em:
+https://medium.com/@xaviergeerinck/how-to-bind-c-code-with-dotnet-core-157a121c0aa6
+
+### Passos:
+Somente com os fontes e o CMakeList.txt, executar os seguintes comandos:
+
+#### 1.Configure the MakeFile
+cmake .
+
+#### 2.Build with all cores available
+- MacOS: 
+    - make -j$(sysctl hw.ncpu | cut -d: -f2)
+- Linux: Build with all cores available
+    - make -j$(grep -c ^processor /proc/cpuinfo)
---
+++ dotnet/Program.cs
@@ -0,0 +1,31 @@
+﻿// using System;
+
+// namespace dotnet
+// {
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             Console.WriteLine("Hello World!2222");
+//         }
+//     }
+// }
+
+
+
+using System;
+using System.Runtime.InteropServices;
+
+namespace CppBind
+{
+    class Program
+    {
+        [DllImport(@"../cpp/libhello-cpp.so")]
+        public static extern void PrintHelloWorld();
+
+        static void Main(string[] args)
+        {
+            PrintHelloWorld();
+        }
+    }
+}
