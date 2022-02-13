26dd1c2c5620712311e83a3735520b3c99aa59de
leobeckerdaluz <leobeckerdaluz@gmail.com>
2022-02-13 15:23:07 -0300

    Update script.sh + CEC2017cpp path

--- cpp/cmake_install.cmake
+++ cpp/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/lbluz/Documents/GEO_OptimizationAlgorithms/CEC2017_linux_cpp_csharp_integration/cpp
+# Install script for directory: /home/lbluz/Documents/GEO_OptimizationAlgorithms/src/CEC2017_linux_cpp_csharp_integration/cpp
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/lbluz/Documents/GEO_OptimizationAlgorithms/CEC2017_linux_cpp_csharp_integration/cpp/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/lbluz/Documents/GEO_OptimizationAlgorithms/src/CEC2017_linux_cpp_csharp_integration/cpp/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
